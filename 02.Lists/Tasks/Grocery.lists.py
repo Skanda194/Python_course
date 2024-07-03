06c7ddd5aea05b1ca6dd7417840849d00f3fb301
Skanda194 <skanda.h@oldorchardschool.com>
2024-07-02 18:59:11 -0700

    Add files via upload

---
+++ 02.Lists/Tasks/Grocery.lists.py
@@ -0,0 +1,14 @@
+myList=["milk", "eggs", "bread", "butter", "cheese"]
+print(myList)
+#myList2=["apples","bananas","orange juice"]
+#myList.append(myList2)
+myList.append("apple")
+print(myList)
+#myList.remove("milk")
+#myList.remove("eggs")
+print(len(myList))
+myList[0]="cheaper milk"
+print(myList)
+print(myList[0],myList[1],myList[2])
+print(myList[0:3])
+print(myList[-3:-1])
