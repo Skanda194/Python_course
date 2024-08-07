095fe6721b964dad91eaeb17cd0f44fa73b701d4
Arvinder Pal <arvinderpal10@gmail.com>
2024-08-07 07:50:46 +0530

    Functions Exercise in Python

---
+++ Day9/0051.Exercise.py
@@ -0,0 +1,116 @@
+
+"""
+1. Sum of Two Numbers
+   -  Problem:  Write a function  sum_of_two(a, b)  that returns the sum of two numbers  a  and  b .
+   -  Input Example:  sum_of_two(3, 5) 
+   -  Output Example:  8 
+
+2.  Check Even or Odd 
+   -  Problem:  Write a function  is_even(number)  that returns  True  if the given number is even, otherwise  False .
+   -  Input Example:  is_even(4) 
+   -  Output Example:  True 
+
+3.  Find Maximum of Two Numbers 
+   -  Problem:  Write a function  find_max(a, b, c)  that returns the maximum of three numbers.
+   -  Input Example:   find_max(3, 7, 5) 
+   -  Output Example:   7 
+
+4.  Convert Celsius to Fahrenheit 
+   -  Problem:  Write a function  celsius_to_fahrenheit(celsius)  that converts a temperature from Celsius to Fahrenheit.
+   -  Input Example:   celsius_to_fahrenheit(0) 
+   -  Output Example:   32.0 
+
+5.  Find the Length of a String 
+   -  Problem:  Write a function  string_length(s)  that returns the length of the given string  s .
+   -  Input Example:   string_length("hello") 
+   -  Output Example:   5 
+
+6.  Convert Kilometers to Miles 
+    -  Problem:  Write a function  km_to_miles(km)  that converts a distance in kilometers to miles. (1 km = 0.621371 miles)
+    -  Input Example:   km_to_miles(10) 
+    -  Output Example:   6.21371 
+
+7.  Calculate Area of a Circle 
+    -  Problem:  Write a function  area_of_circle(radius)  that returns the area of a circle given its radius.
+    -  Input Example:   area_of_circle(5) 
+    -  Output Example:   78.54  (Use  π ≈ 3.1416 )
+
+8. Product of three  of Numbers
+   -  Problem:  Write a function  product_of_three(a, b, c)  that returns the product of three numbers   a , b and c .
+   -  Input Example:  product_of_three(2, 4,5) 
+   -  Output Example:  40
+
+9. Calculate Average of a List 
+   -  Problem:  Write a function  calculate_average(numbers)  that returns the average of a list of numbers.
+   -  Input Example:   calculate_average([10, 20, 30, 40]) 
+   -  Output Example:   25.0 
+
+10.  Reverse a String 
+    -  Problem:  Write a function  reverse_string(s)  that returns the reverse of the given string  s .
+    -  Input Example:   reverse_string("python") 
+    -  Output Example:   "nohtyp" 
+
+11.  Count Vowels in a String 
+    -  Problem:  Write a function  count_vowels(s)  that returns the number of vowels in the given string  s .
+    -  Input Example:   count_vowels("hello") 
+    -  Output Example:   2 
+
+
+
+13.  Check Palindrome 
+    -  Problem:  Write a function  is_palindrome(s)  that returns  True  if the string  s  is a palindrome, otherwise  False .
+    -  Input Example:   is_palindrome("racecar") 
+    -  Output Example:   True 
+
+14.  Find Minimum of a List 
+    -  Problem:  Write a function  find_minimum(numbers)  that returns the minimum value from a list of numbers.
+    -  Input Example:   find_minimum([3, 1, 4, 1, 5]) 
+    -  Output Example:   1 
+
+
+17.  Count Words in a String 
+    -  Problem:  Write a function  count_words(sentence)  that returns the number of words in a given sentence.
+    -  Input Example:   count_words("Hello world") 
+    -  Output Example:   2 
+ 
+
+19.  Sum of List Elements 
+    -  Problem:  Write a function  sum_list(numbers)  that returns the sum of all elements in a list.
+    -  Input Example:   sum_list([1, 2, 3, 4]) 
+    -  Output Example:   10 
+
+20.  Count Digits in a Number 
+    -  Problem:  Write a function  count_digits(n)  that returns the number of digits in a given integer  n .
+    -  Input Example:   count_digits(12345) 
+    -  Output Example:   5 
+
+21.  Find GCD (Greatest Common Divisor) 
+    -  Problem:  Write a function  find_gcd(a, b)  that returns the greatest common divisor of two numbers.
+    -  Input Example:   find_gcd(12, 15) 
+    -  Output Example:   3 
+
+22.  Check Armstrong Number 
+    -  Problem:  Write a function  is_armstrong(n)  that returns  True  if the number  n  is an Armstrong number, otherwise  False .
+    -  Input Example:   is_armstrong(153) 
+    -  Output Example:   True 
+
+23.  Remove Duplicates from List 
+    -  Problem:  Write a function  remove_duplicates(numbers)  that returns a new list with duplicates removed from the given list.
+    -  Input Example:   remove_duplicates([1, 2, 2, 3, 4, 4, 5]) 
+    -  Output Example:   [1, 2, 3, 4, 5] 
+
+24.  Sort a List 
+    -  Problem:  Write a function  sort_list(numbers)  that returns the list sorted in ascending order.
+    -  Input Example:   sort_list([4, 2, 3, 1]) 
+    -  Output Example:   [1, 2, 3, 4] 
+
+25.  Check Anagram 
+    -  Problem:  Write a function  is_anagram(str1, str2)  that returns  True  if the strings  str1  and  str2  are anagrams, otherwise  False .
+    -  Input Example:   is_anagram("listen", "silent") 
+    -  Output Example:   True 
+
+---
+
+
+
+"""
