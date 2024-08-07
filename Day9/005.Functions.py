2fdf748c6dd96ddc67f07d7364abfbd7791ddab3
Arvinder Pal <arvinderpal10@gmail.com>
2024-08-07 07:28:08 +0530

    Functions in Python

---
+++ Day9/005.Functions.py
@@ -0,0 +1,52 @@
+# Functions are block of code - 
+# Function definition - def 
+def PrintDetails():
+    pass
+
+for i in range(5):
+    pass 
+
+# Function Call - For a function to exceute we have to call it. 
+# PrintDetails()
+
+# Arguments - While calling I will supply some data(variables)
+#PrintDetails("Arvinder", "Software Guy with Teaching exp",27)
+
+# Parameters - These are tyhe values in function definition 
+
+# Function definition recieving parameters - 10 , 30
+# def Add(a,b):
+#     print(a+b)
+
+# Add(10,30)   
+# Calling a function - with arguments 10 , 30
+
+# def Add(a,b):
+#     print(10+50)
+
+# Add(10,30) 
+
+# def UserDetails(name,schools):
+#     print(name)
+#     print(schools)
+
+# UserDetails("Skanda Harita", "OOS")
+
+# def rectangleArea(l,b):
+#     print("The area of rectangle is :",l*b)
+
+# len = 30
+# bre = 10
+# rectangleArea(len, bre)
+
+
+# def StudentDetails(name,std):
+#     print("My name is :", name,". I study in",std)
+
+
+# StudentDetails("Arvinder","9th")
+
+
+
+
+
