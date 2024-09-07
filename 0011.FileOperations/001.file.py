2bac46bb37dae545d168cb313e5ffecd876963f2
Arvinder Pal <arvinderpal10@gmail.com>
2024-09-07 22:23:46 +0530

    Python file operations

---
+++ 0011.FileOperations/001.file.py
@@ -0,0 +1,20 @@
+import os  #Operating System 
+
+f = open('test.txt','r')
+# filename , mode 
+
+# TO read the whole content
+# print(f.read())
+
+# to read first character lines of the file
+# print(f.read(5))
+
+# print(f.readline())
+# print(f.readline())
+
+
+for x in f:
+    print(x)
+
+
+f.close()
---
+++ 0011.FileOperations/002.Write.py
@@ -0,0 +1,8 @@
+import os 
+
+# f = open('test2.txt','w')
+f = open('test2.txt','a')
+
+f.write("This is my second line")
+
+f.close()
