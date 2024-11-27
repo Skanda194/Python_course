ad935ed2b57958c8b08294bbbc6cdf6f3f87dd4f
Arvinder Pal <arvinderpal10@gmail.com>
2024-11-27 07:40:31 +0530

    Python OPerators - Arithmetic

---
+++ 014.Operators/001.ArithmeicOp.py
@@ -0,0 +1,51 @@
+# Operators in Python - THese are used to perform operations on variables and values.
+
+# Arithmetic Operators - These are used to perform mathematical operations. 
+# Addition - + +> 5 + 3 = 8 
+print("Addition:",5+3)
+
+# Subtraction - - > 5 - 3 = 2
+print("Subtraction :", 5-3)
+
+# Multiplication - * > 5 * 3 = 15
+print("Multiplication:", 5*3)
+# Division - / > 5 / 3 = 1.6666666666666667
+print("Division (Complete) :", 16/3)
+# Floor (Integer) Division - // > 5 // 3 = 1 
+print("Floor division :",16//3) 
+# Exponentiation - ** > 5 ** 3 = 125  -- Math Module -- Pow()
+print("Exponentiation :", 5**3)
+# Modulus (Remainder) - % > 5 % 3 = 2
+print("Modulus (Remainder) Operator: ",17%3)
+
+
+
+print("---------------------------------- Combined Operator ---------------------------------")
+# Combined Operators :
+# 
+# += 
+x =10 
+x += 5    # x = x + 5
+print("x +=",x)
+
+y =30
+y -= 5    # x = x - 5
+print("x -=",y)
+
+z = 10
+z *= 5    # x = x * 5
+print("x *=",z)
+
+x =20
+x /= 5    # x = x / 5
+print("x /=",x)
+
+x = 12
+x %= 5    # x = x % 5
+
+print("x %=",x)
+
+
+
+
+
