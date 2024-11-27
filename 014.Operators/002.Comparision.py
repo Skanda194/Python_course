df12511caad4792741d9f769df141aba0bf22b43
Arvinder Pal <arvinderpal10@gmail.com>
2024-11-27 07:41:15 +0530

    Python Operators- COmparision

---
+++ 014.Operators/002.Comparision.py
@@ -0,0 +1,29 @@
+# Comparision Operators : 
+# 1. Equal to : a == b
+print(3==3)
+print(5==4)
+# 2. Not Equal to : a != b  # a is not equal to b
+print(3!=3)
+print(5!=4)
+# 3. Smalller than : a < b  # a is smaller than b
+print(3<5)
+print(5<4)
+# 4. Greater than : a > b  # a is greater than b
+print(3>5)
+print(5>4)
+
+# Combined OPerators :
+# >= : Greater than or equal to : a >= b
+
+x =18
+if x>=18:
+    print("You can drive.")
+else:
+    print("You can't drive.")
+
+
+x =17
+if x<=18:
+    print("You can't drive.")
+else:
+    print("You can drive.")
