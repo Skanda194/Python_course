78b3ee87a73208f43cbebf281cdaceeaaebcf07c
Arvinderpal10 <arvinderpal10@gmail.com>
2025-01-17 07:54:08 +0530

    Formatted strings in Python

---
+++ 015.StringFormating/001.Print.py
@@ -0,0 +1,48 @@
+# print("Hello")
+# print("Something")
+# print(345)
+
+#Every print starts on a new line
+# print("Hello", end='*')
+# print("Something",end='*')
+# print(345, end='*')
+
+
+#Formattted Strings 
+
+x = 4456
+IsGraduated = True
+name = "Mr. Hello"
+print("Hi My name is :",name," Are you graduate ?",IsGraduated, "My score is :")
+
+
+print(f'Hi My name is {name} Are you graduate ? {IsGraduated} My score is : {x}')
+
+# he said, "I will no be join to join the call."
+# print("he said, "I will no be join to join the call."")
+
+print('he said, "I will no be join to join the call."')
+
+
+# he said, 'I will no be join to join the call.'
+
+print("he said, 'I will no be join to join the call.'")
+
+# he said, "I will no be join to join the call hs's some meeting to attend"
+
+'''
+I  can write anything here.
+'''
+print('''he said, "I will no 
+      be join to join 
+      the call hs's some meeting to attend"''')
+print("""he said, "I will no be join to join the call hs's some meeting to attend""")
+
+
+# Escape Symbols : \ 
+
+print('he said, \'I will no be join to join the call.\'')
+print("he said, \"I will no be join to join the call.\"")
+
+
+print("he said, \"I will no be join to join the call hs's some meeting to attend\"")
