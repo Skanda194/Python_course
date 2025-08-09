e1ba860d2b09305dc552cf8c61f8d34de6b787a3
ArvinderPal10 <arvinderpal10@gmail.com>
2025-08-09 22:49:03 +0530

    Python Codes

---
+++ Practice/return.py
@@ -0,0 +1,11 @@
+# Return Keyword
+
+def AddNumber(a,b):
+    # print(a+b)
+    result = 0
+    result = a+b
+    return result
+    # print("This will not execute")
+
+print(AddNumber(10,20))
+    
