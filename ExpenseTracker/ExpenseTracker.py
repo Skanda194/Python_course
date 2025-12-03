b2e28ece36c8457e772ab34fc10153a243c3bd6a
Skanda194 <skanda.h@oldorchardschool.com>
2025-12-02 18:06:41 -0800

    on 12/2/25 ExpenseTracker.py has been pushed

--- ExpenseTracker/ExpenseTracker.py
+++ ExpenseTracker/ExpenseTracker.py
@@ -1,6 +1,3 @@
-from Practice.NestedAPIs import myData
-
-
 def account():
     print("Welcome to Expense tracker! Please create an account to continue")
     account=[]
@@ -25,14 +22,15 @@ def entry():
         date=input("Enter date ")
         category={1:["business"],2:["education"],3:["grocery"],4:["extra"],}
         print(category)
-        my_category=int(input("Enter number which corresponds to which category is your purchase: "))
-        total=0
-        for item in category:
-            total+=str(item[1])
-    my_account_entry.append(account_entry)
-    my_account_entry.append(date)
-    my_account_entry.append(total)
-
+        my_category = int(input("Enter number which corresponds to which category is yours: "))
+        total = category[my_category][0]
+        my_account_entry.append(account_entry)
+        my_account_entry.append(date)
+        my_account_entry.append(total)
+        print("Summary:",my_account_entry)
+        length=len(my_account_entry)
+    for i in range(1,length+1):
+        print("Total cost:",my_account_entry[0]+my_account_entry[1])
 
 
 
