c46bf9d6970a4eb71cec1b56dc0279191557cf9a
Arvinder Pal <arvinderpal10@gmail.com>
2025-12-03 07:38:52 +0530

    Modulkes in Python

---
+++ 010.Modules/Revision/first.py
@@ -0,0 +1,45 @@
+# Modules --- 
+
+# Advanced bill Splitter -- SPlit the code into multiple files (import)
+
+# Advanced Bill Splitter
+
+# Bill Splitter 
+#    order.py 
+#    menu.py 
+#    bill.py
+#    tax.py 
+#    tip.py
+#    main.py 
+
+# # Modules --Built in modules -- os , json , math , datetime , random 
+# import math 
+# print(math.sqrt(25))
+# User created module -- It's just a .py file 
+
+# import myFirstModule
+# print(myFirstModule.Greet())
+# print(myFirstModule.MAX_CALLS)
+
+# from math import sqrt 
+# print(sqrt(45))
+
+
+# from myFirstModule import Greet
+# print(Greet())
+
+# Alias 
+# import pandas as pd 
+# import myFirstModule as fm
+# print(fm.MAX_CALLS)
+# print(fm.x)
+
+# Pacakge -- __init__.py
+
+# Library -- 
+
+# pip -- python package installer 
+# pip intall requests 
+
+
+
